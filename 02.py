1e02e31abce92369f39f68b909342625af7d054d
Judith <judithvdw@users.noreply.github.com>
2023-12-02 09:38:22 +0100

    day 2

---
+++ 02.py
@@ -0,0 +1,50 @@
+def parse(raw_data):
+    all_games = {}
+    for game in raw_data:
+        header, info = game.split(": ")
+        game_nr = int(header.split()[1])
+        cube_sets = []
+        for game_round in info.split("; "):
+            for colour in game_round.split(", "):
+                n, col = colour.split()
+                cube_sets.append((int(n), col))
+        all_games[game_nr] = cube_sets
+    return all_games
+
+
+def part1(all_games):
+    total = 0
+    RED, GREEN, BLUE = 12, 13, 14
+    for nr, cubes in all_games.items():
+        for cube in cubes:
+            if (cube[1] == "blue" and cube[0] > BLUE) or (cube[1] == "green" and cube[0] > GREEN) or (
+                    cube[1] == "red" and cube[0] > RED):
+                break
+        else:
+            total += nr
+    return total
+
+
+def part2(all_games):
+    powers = []
+    for nr, cubes in all_games.items():
+        reds = []
+        blues = []
+        greens = []
+        for cube in cubes:
+            if cube[1] == "blue":
+                blues.append(cube[0])
+            if cube[1] == "green":
+                greens.append(cube[0])
+            if cube[1] == "red":
+                reds.append(cube[0])
+        powers.append(max(reds) * max(greens) * max(blues))
+    return sum(powers)
+
+
+with open("inputs/02.txt") as f:
+    raw_data = f.readlines()
+    all_games = parse(raw_data)
+
+print(f"Part 1: {part1(all_games)}")
+print(f"Part 2: {part2(all_games)}")
