d277ad29649538b97c020407f0d530496e7610eb
Judith <judithvdw@users.noreply.github.com>
2023-12-06 11:32:22 +0100

    nu ook beschikbaar met wiskunde

--- 06.py
+++ 06.py
@@ -1,15 +1,17 @@
 import math
 
 
-def find_winning_strategies(data):
-    for duration, record in data:
-        distances = [speed * (duration - speed) for speed in range(duration)]
-        winning = list(filter(lambda x: x > record, distances))
-        yield len(winning)
+def num_winning(stats):
+    duration, record = stats
+    a = -1
+    b = duration
+    c = -record
+    low, high = ((-b + math.sqrt(b ** 2 - 4 * a * c)) / 2 * a), ((-b - math.sqrt(b ** 2 - 4 * a * c)) / 2 * a)
+    return math.floor(high) - math.floor(low)
 
 
 part_1 = [(57, 291), (72, 1172), (69, 1176), (92, 2026)]
 part_2 = [list(map(lambda x: int(''.join([str(i) for i in x])), list(zip(*part_1))))]  # lol, met de hand was sneller
 
-print(math.prod(find_winning_strategies(part_1)))
-print(math.prod(find_winning_strategies(part_2)))
+print(f"Part 1: {math.prod(list(map(num_winning, part_1)))}")
+print(f"Part 2: {math.prod(list(map(num_winning, part_2)))}")
